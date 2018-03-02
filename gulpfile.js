141bc577d6fdc4d8d6b8cc4d241ef293170256c1
shatko <mladen.salaj@gmail.com>
2018-03-02 11:43:31 +0100

    Sets sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 // Gulpfile
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// Adds sass compiler
+gulp.task('sass', function() {
+  gulp.src('styles/main.scss')
+  .pipe(sass({style: 'expanded'}))
+  .pipe(gulp.dest('gulp-compiled/css'))
+});
