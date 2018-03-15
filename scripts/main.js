00b69760c57b901b420bf359668e0a262a8315af
shatko <mladen.salaj@gmail.com>
2018-03-15 16:34:49 +0100

    Starts Isotope

--- scripts/main.js
+++ scripts/main.js
@@ -11,9 +11,48 @@ $('.mobile-x').click(function(){
 });
 
 
-
 // index page Slick slider
 $('.slider-container').slick({
   nextArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button"></button>',
   prevArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"></button>'
+});
+
+
+// index page World Slick slider
+$('.world-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  nextArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button"></button>',
+  prevArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"></button>',
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    }
+  ]
+});
+
+
+// index page accordion
+if ($(window).width() <= 768) {
+  $('.accordion-content').removeClass('show');
+}
+$( window ).resize(function() {
+  if ($(window).width() <= 768) {
+    $('.accordion-content').removeClass('show');
+  } else {
+    $('.accordion-content').addClass('show');
+  }
+});
+
+
+// history Isotope
+$('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
 });
